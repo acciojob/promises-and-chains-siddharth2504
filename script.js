fd52927fd4d797c876a4a109f244b7503b11ff63
Siddharth Sisodia <139268988+siddharth2504@users.noreply.github.com>
2025-05-06 00:24:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+let form = document.querySelector("#form")
+let name = document.querySelector("#name")
+let age = document.querySelector("#age")
+
+form.addEventListener("submit", (e) => {
+	e.preventDefault()
+	if(name.value === "" || age.value === ""){
+		alert("Please enter valid details.")
+		return
+	}else{
+		let prom = new Promise((res, rej) => {
+	        if(Number(age.value) > 18){
+	        	setTimeout(() => {
+	        		res("Welcome, You can vote.")
+	        	}, 4000)
+	        }else{
+	        	setTimeout(() => {
+	        		rej("Oh sorry, You aren't old enough.")
+	        	}, 4000)
+	        }
+        })    
+		prom
+		.then(data => alert(data))
+		.catch(data => alert(data))
+	}
+})
+
+
+
